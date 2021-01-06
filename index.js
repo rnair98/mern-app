39d7536a071f1658caac3f604e1519e3ed8b5dcd
Ubuntu <azureuser@azure-linux.tiiyjypcawyubjnsw4frgdh00d.bx.internal.cloudapp.net>
2021-01-06 22:07:58 +0000

    added mongodb connection

---
+++ index.js
@@ -0,0 +1,35 @@
+/*jshint esversion: 6 */
+
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+//export mongodb connection string
+const { MONGODB } = require('./config.js');
+
+const typeDefs = gql`
+    type Query{
+        sayHi: String!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        sayHi: () => 'Hello world!'
+    }
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+mongoose
+    .connect(MONGODB, {useNewUrlParser: true})
+    .then(() => {
+        console.log('MongoDB is connected');
+        return server.listen({ port: 5000 });
+    })
+    .then(res => {
+        console.log(`Server running at ${res.url}`);
+    });
