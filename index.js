e1f57c6d9bad17c6c6f3fc0183a3508e7bede757
rnair98 <nair.rohit98@gmail.com>
2021-01-07 02:14:48 +0000

    defined post object type for graphql

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 /*jshint esversion: 10 */
-
+//dependency imports
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
-//export mongodb connection string
+//relative imports
+const Post = require('./models/Post.js');
 const { MONGODB } = require('./config.js');
 
+//defining graphql types. ! = 'required'
 const typeDefs = gql`
+    type Post{
+        id: ID!
+        body: String!
+        username: String!
+        createdAt: String!
+    }
     type Query{
-        sayHi: String!
+        getPosts = [Post]    
     }
 `;
 
