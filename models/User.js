210274b6367a3ef33f1f391a63825f519483e8d0
rnair98 <nair.rohit98@gmail.com>
2021-01-07 01:49:53 +0000

    finished user model for mongodb

---
+++ models/User.js
@@ -0,0 +1,12 @@
+/*jshint esversion: 10 */
+
+const {model, Schema} = require('mongoose');
+
+const userSchema = new Schema({
+    username: String,
+    password: String,
+    email: String,
+    createdAt: String
+});
+
+module.exports = model('User', userSchema);
